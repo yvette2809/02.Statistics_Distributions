2da217a84c6b047f305eb5df6129c94480ed73f5
Yvette Zhang <yz2809@columbia.edu>
2020-12-12 09:27:40 -0500

    Update general_dstn.py

--- distributions/general_dstn.py
+++ distributions/general_dstn.py
@@ -1,1 +1,10 @@
-
+class Distribution:
+	
+	def __init__(self, mu=0, sigma=1):
+		
+		"""
+		Generic distribution class for calculating and visualizing a probability distribution.
+		
+		Attributes:
+		
+		"""
